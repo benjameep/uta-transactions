3bb7ea202ae9d7c3bcf0fb50403baa73f937a576
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2025-11-17 11:54:53 -0700

    style per day

--- app.py
+++ app.py
@@ -27,9 +27,12 @@ def fetch_transactions(card_number):
     return r.text
 
 st.title("UTA Card Transactions")
-card_number = st.text_input("Enter your UTA Card Number", value=st.query_params.get("card"))
-if not card_number:
-    st.stop()
+if not st.query_params.get("card"):
+    card_number = st.text_input("Enter your UTA Card Number", value=st.query_params.get("card"))
+    if not card_number:
+        st.stop()
+else:
+    card_number = st.query_params.get("card")
 
 with st.spinner("Fetching transactions..."):
     html = fetch_transactions(card_number)
@@ -57,11 +60,24 @@ df['time'] = pd.to_datetime(df.date)
 # If it happend at midnight, pretend it happened the day before
 df['time'] = (df.time - timedelta(hours=1)).apply(lambda ts: ts.replace(minute=59)).where(df.time.dt.hour == 0, df.time)
 df['date'] = df.time
+date = df.time.dt.date
+df['_group'] = (date != date.shift()).cumsum()
 df['amount'] = df.amount.str.replace('$','').astype(float)
 df['balance'] = ((-df.amount).cumsum() + BALANCE).shift(fill_value=BALANCE)
 
+def color_groups(row):
+    if row['_group'] % 2 == 0:
+        return ['background-color: #f0f0f0'] * len(row)
+    else:
+        return ['background-color: white'] * len(row)
+
 st.dataframe(
-    df[['date', 'time','amount','balance']].sort_values('time', ascending=False),
+    (
+        df.query('amount < 0')
+        [['date', 'time', 'balance', '_group']]
+        .sort_values('time', ascending=False)
+        .style.apply(color_groups, axis=1)
+    ),
     height=800,
     hide_index=True,
     column_config={
@@ -77,17 +93,12 @@ st.dataframe(
             format="h:mm a",
             help="The time the transaction occurred.",
         ),
-        'amount': st.column_config.NumberColumn(
-            "Amount",
-            width='medium',
-            format="dollar",
-            help="The amount of the transaction. Negative values indicate money spent.",
-        ),
         'balance': st.column_config.NumberColumn(
             "Balance",
             format="dollar",
             help="The balance on the card after the transaction.",
         ),
+        '_group': None,
     }
 )
         
